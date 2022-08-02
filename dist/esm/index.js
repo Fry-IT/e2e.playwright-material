a5c50d596109e09c15cb400a2de0e730ad5f4793
Pavel Kunc <pavel.kunc@gmail.com>
2022-08-02 15:09:58 +0200

    Modernise package and make it ready for GitHub Packages

--- dist/esm/index.js
+++
@@ -1,3 +0,0 @@
-export * from './playwright-element.js';
-export * from './playwright-harness-environment.js';
-//# sourceMappingURL=index.js.map
--- dist/types/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './playwright-element.js';
-export * from './playwright-harness-environment.js';
